e522f2c7df0cae0142aca6a65320ace3f0fbaf2f
mgt.20 <morteza.ghazitehrani@gmail.com>
2020-05-30 18:43:23 -0700

    Updating script with config section

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ A simple python3 script that sends an e-mail alert if a desired item is on a web
 
 To Use:
 
-1. Identify the webpage to scrap
+1. Identify the webpage to scrape
 2. Use the Inspector feature in a web browser to find the CSS element that the text resides in. 
 3. Modify the python script by replacing the variables after the "$"'s with your variables (email addresses, webpage url, css selector). Note: delete the $'s
 4. Provide your sender account email address credentials, the script is pre-configured with gmail smtp information 
--- script.py
+++ script.py
@@ -1,38 +1,45 @@
 #! python3
 import bs4, requests, smtplib, logging
 
-# ------------------- E-mail list ------------------------
-toAddress = ['$destinationemailaddress@gmail.com']
-# --------------------------------------------------------
-
-webpage = '$https://webpageyouwanttoscrape.xyz/index.html'
-
 logging.basicConfig(filename='log.log', filemode='w', level=logging.DEBUG)
 
-#Download page
+## Configure these variables:
+toAddress = ['$senttheemailtothisaddress@gmail.com']
+# This is the email address you want to notify when there is a match
+webpage = '$http://www.whateverwebsiteyouwant.xyz/items.html'
+# This is the webpage you want to scrape
+cssselector = '.$whatevercssselector-com-category-product-name'
+# This is the CSS selector from the webpage where the keyword lives. It should begin with a "."
+searchterm = '$whatwordshouldwelookfor'
+# This is the keyword you are looking to match on the webpage
+fromAddress = '$sendtheemailfromthisaddress@gmail.com'
+# This is the email address we want to send the email from
+fromPassword = '$mypasswordORappkey'
+# This is the password or gmail app key (preferred) for the email address we are mailing from 
+## End of Configuration
+
 getPage = requests.get(webpage)
 getPage.raise_for_status() #if error it will stop the program
 
 #Parse text for keyword
 menu = bs4.BeautifulSoup(getPage.text, 'html.parser')
-keywords = menu.select('$cssselectorfrominspectingwebpage')
+keywords = menu.select(cssselector)
 
-the_one = '$keywordtosearchfor' # This is the keyword to scrape the webpage for
-flength = len(the_one)
+flength = len(searchterm)
 available = False
 
 for keyword in keywords:
-    for i in range(len(keywords.text)):
+    for i in range(len(keyword.text)):
         chunk = keyword.text[i:i+flength].lower()
-        if chunk == the_one:
+        if chunk == searchterm:
             available = True
 
 if available == True:
     conn = smtplib.SMTP('smtp.gmail.com', 587) # smtp address and port
     conn.ehlo() # call this to start the connection
     conn.starttls() # starts tls encryption. When we send our password it will be encrypted.
-    conn.login('$senderemailaddress@gmail.com', '$senderemailpasswordORappkey')
-    conn.sendmail('youremail@gmail.com', toAddress, 'Subject: $This is the email subject!\n\nAttention!\n\nYour favourite item is available today!\n\nVisit ' + (webpage) + ' to see it\n\nNotifier V1.0')
+    conn.login(fromAddress, fromPassword)
+    conn.sendmail(fromAddress, toAddress, 'Subject: python3-webscraper-emailer alert!\n\nAttention!\n\nYour favorite item is available today!\n\nVisit ' + (webpage) + ' to see it\n\npython3-webscraper-emailer V1.0')
     conn.quit()
     print('Sent notificaton e-mails for the following recipients:\n')
     for i in range(len(toAddress)):
